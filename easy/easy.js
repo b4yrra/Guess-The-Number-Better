c0a80488fc48c60d3e01372885762a9864ec3848
b4yrra <t.chinbayr@gmail.com>
2025-11-28 18:42:21 +0800

    guess

---
+++ easy/easy.js
@@ -0,0 +1,49 @@
+document.getElementById("backBtn").onclick = () => {
+  location.href = "../index.html";
+};
+
+let attempts = 0;
+let running = true;
+let value = Math.floor(Math.random() * 10) + 1;
+
+const guess = document.getElementById("guess");
+const submit = document.getElementById("submitBtn");
+const hint = document.getElementById("hint");
+const attemptsText = document.getElementById("attempts");
+const popup = document.getElementById("popup");
+const popupMessage = document.getElementById("popupMessage");
+const closePopup = document.getElementById("closePopup");
+const overlay = document.getElementById("overlay");
+
+submit.addEventListener("click", checkGuess);
+closePopup.addEventListener("click", () => {
+  popup.classList.remove("show");
+  overlay.classList.remove("show");
+});
+
+function showPopup(message) {
+  popupMessage.textContent = message;
+  popup.classList.add("show");
+  overlay.classList.add("show");
+}
+
+function checkGuess() {
+  const userValue = Number(guess.value);
+  attempts++;
+
+  if (userValue < 1 || userValue > 10) {
+    window.alert(`Please pick a number between 1 to 10`);
+    return;
+  }
+
+  if (userValue === value) {
+    hint.textContent = "Congratulation, you won!";
+    showPopup("You won! 🎉");
+  } else if (userValue < value) {
+    hint.textContent = "Too low, try again!";
+  } else if (userValue > value) {
+    hint.textContent = "Too high, try again!";
+  }
+
+  attemptsText.textContent = "Attempts: " + attempts;
+}
---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("easyBtn").onclick = () => {
+  location.href = "easy/easy.html";
+};
+
+document.getElementById("mediumBtn").onclick = () => {
+  location.href = "easy/medium.html";
+};
+
+document.getElementById("hardBtn").onclick = () => {
+  location.href = "easy/hard.html";
+};
